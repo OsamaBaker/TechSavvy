bb65509d5e06f824a8e9c8cd1b84774a170ce331
Osama Baker <edewanjy@hotmail.com>
2021-03-22 13:56:09 +0200

    Included the JS code in functions

--- javascript/app.js
+++ javascript/app.js
@@ -4,40 +4,57 @@ var greeting = document.write('<h3>'+'Hello ' +firstName+ '</h3>')
 
 var favouriteColor = prompt("What is your favourite color?");
 
-while(favouriteColor!=='red' && favouriteColor!=='blue') {
-    favouriteColor = prompt('Please choose between red and blue');
+function getFavouriteColor(favouriteColor){
+
+    while(favouriteColor!=='red' && favouriteColor!=='blue') {
+        favouriteColor = prompt('Please choose between red and blue');
+    }
+    
+    if (favouriteColor == 'red'){
+        document.write('<img src="images/red-box.webp">');
+    } else if (favouriteColor == 'blue'){
+        document.write('<img src="images/blue-box.png"/');
+    } else {
+        alert("Choose between blue and red");
+    }
+
 }
 
-if (favouriteColor == 'red'){
-    document.write('<img src="images/red-box.webp">');
-} else if (favouriteColor == 'blue'){
-    document.write('<img src="images/blue-box.png"/');
-} else {
-    alert("Choose between blue and red");
+var getNumberOfBoxes = function () {
+
+    var numberOfBoxes = prompt('how many boxes do you want?');
+    
+    var boxes = '';
+    var result = '';
+    
+    if (favouriteColor == 'red') {
+        boxes = '<img src="images/red-box.webp">';
+    } else if (favouriteColor == 'blue') {
+        boxes = '<img src="images/blue-box.png"/';
+    }
+    
+    for (var i = 0; i < numberOfBoxes; i++) {
+        result += boxes;
+    }
+    
+    document.write(result)
+
 }
 
-var numberOfBoxes = prompt('how many boxes do you want?');
+getNumberOfBoxes();
 
-var boxes = '';
-var result = '';
+var getFavouritePet = function (){
 
-if (favouriteColor == 'red') {
-    boxes = '<img src="images/red-box.webp">';
-} else if (favouriteColor == 'blue') {
-    boxes = '<img src="images/blue-box.png"/';
+    var favouritePet = prompt("What is your favourite pet?")
+    
+    if (favouritePet == 'cat'){
+        document.write('<img src="images/cat.png"/');
+    } else if(favouritePet == 'dog'){
+        document.write('<img src="images/dog.jpg"/');
+    } else{
+        confirm("Choose between a cat or a dog, and refresh the page");
+    }
+
 }
 
-for (var i = 0; i < numberOfBoxes; i++) {
-    result += boxes;
-}
-
-document.write(result)
-var favouritePet = prompt("What is your favourite pet?")
-
-if (favouritePet == 'cat'){
-    document.write('<img src="images/cat.png"/');
-} else if(favouritePet == 'dog'){
-    document.write('<img src="images/dog.jpg"/');
-} else{
-    confirm("Choose between a cat or a dog, and refresh the page");
-}
+getFavouritePet();
